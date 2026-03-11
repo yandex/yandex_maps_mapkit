1b9d95e41923cfbb4e60a1f86bd4d53b9e2f5f02
YandexMapKit <robot-mapkit-ci@yandex-team.ru>
2026-03-11 23:26:26 +0300

    Release 4.32.1-beta

--- lib/src/mapkit/traffic/traffic_listener_export.dart
+++ lib/src/mapkit/traffic/traffic_listener_export.dart
@@ -1,7 +1,7 @@
 export 'package:yandex_maps_mapkit/src/mapkit/traffic/traffic_listener.dart'
     hide
         TrafficListenerImpl,
+        TrafficLevelNative,
         TrafficLevelContainerExtension,
         TrafficColorImpl,
-        TrafficLevelImpl,
         TrafficColorContainerExtension;
